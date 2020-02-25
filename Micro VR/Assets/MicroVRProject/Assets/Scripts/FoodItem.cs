020f4a3624fa890a6bad38c75ad08a2300ed8751
Mitchell <mitchell.p031@stu.nebo.edu>
2020-02-25 10:27:11 -0700

    borgger

--- Micro VR/Assets/MicroVRProject/Assets/Scripts/FoodItem.cs
+++ Micro VR/Assets/MicroVRProject/Assets/Scripts/FoodItem.cs
@@ -16,13 +16,13 @@ public class FoodItem : MonoBehaviour
             mr.material.color = Color.Lerp(mr.material.color, Color.black, cookRate * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("CookSurface"))
             isCooking = true;
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider collision)
     {
         if(collision.gameObject.CompareTag("CookSurface"))
             isCooking = false;
